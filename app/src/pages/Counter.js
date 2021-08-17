6c59b2be227e0ccb07e956bd95db7c6565bbf827
radiographer <lukas.w@gmx.com>
2021-08-17 16:53:42 +0200

    counter in progess

--- app/src/pages/Counter.js
+++ app/src/pages/Counter.js
@@ -1,16 +1,43 @@
 import React, { useState, useEffect } from "react";
+import { Button, Box } from "@material-ui/core";
 import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Container = styled.div`
-  background-color: #dbf1ee;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 80vh;
-`;
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: "#dbf1ee",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "80vh",
+  },
+
+  button: {
+    color: "white",
+    backgroundColor: "#77a8a8",
+
+    fontFamily: "Montserrat",
+    fontSize: 20,
+    fontWeight: 500,
+    fontStyle: "italic",
+    textDecoration: "none",
+  },
+  result: {
+    backgroundColor: "white",
+    border: "1px solid",
+    borderRadius: 5,
+    width: 100,
+    fontFamily: "Montserrat",
+    fontSize: 20,
+    fontWeight: 500,
+    fontStyle: "italic",
+    textDecoration: "none",
+  },
+}));
 
 function Counter({ defaultValue = 0, step = 1 }) {
   const [value, setValue] = useState(defaultValue);
+  const classes = useStyles();
 
   useEffect(() => {});
 
@@ -21,12 +48,27 @@ function Counter({ defaultValue = 0, step = 1 }) {
   const onPlus = (e) => {
     setValue(value + step);
   };
+
+  const onReset = (e) => {
+    setValue(defaultValue);
+  };
   return (
-    <Container>
-      <button onClick={onMinus}>-</button>
-      {value}
-      <button onClick={onPlus}>+</button>
-    </Container>
+    <div className={classes.root}>
+      <Box>
+        {" "}
+        <Button className={classes.button} onClick={onPlus}>
+          plus 1
+        </Button>
+        <div className={classes.result}>{value}</div>
+        <Button className={classes.button} onClick={onMinus}>
+          minus 1
+        </Button>
+      </Box>
+
+      <Button className={classes.button} onClick={onReset}>
+        Reset
+      </Button>
+    </div>
   );
 }
 
