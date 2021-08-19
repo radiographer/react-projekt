b5c4b72c40080dffdb1ec965eaf7a40a77bb5abc
radiographer <lukas.w@gmx.com>
2021-08-19 10:32:37 +0200

    characters begin

--- app/src/pages/Characters.js
+++ app/src/pages/Characters.js
@@ -1,22 +1,94 @@
+// import React, { useState, useEffect } from "react";
+// import ListaPostaci from "./components/ListaPostaci";
+
+// function App() {
+//   const [postaci, setPostaci] = useState(null);
+
+//   useEffect(() => {
+//     fetch("https://rickandmortyapi.com/api/character")
+//       .then((response) => response.json())
+//       .then(setPostaci);
+//   }, []);
+
+//   if (!postaci) {
+//     return <div className="App">Brak danych z backandu</div>;
+//   }
+//   return (
+//     <div className="App">
+//       <ListaPostaci postaci={postaci} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "../App.css";
 import React, { useState, useEffect } from "react";
-import ListaPostaci from "./components/ListaPostaci";
+import Character from "./components/Character";
+import Pagination from "./components/Pagination";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #dbf1ee;
+  margin-top: 200;
+`;
 
 function App() {
-  const [postaci, setPostaci] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://rickandmortyapi.com/api/character"
+  );
+
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [pages, setPages] = useState();
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((response) => response.json())
-      .then(setPostaci);
-  }, []);
+    const url = currentPageUrl;
+    setLoading(true);
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setCharacters(data.results);
+      setLoading(false);
+      setNextPageUrl(data.info.next);
+      setPrevPageUrl(data.info.prev);
+      setPages(data.info.pages);
+    };
+    fetchData();
+  }, [currentPageUrl]);
 
-  if (!postaci) {
-    return <div className="App">Brak danych z backandu</div>;
+  function nextPage() {
+    setCurrentPageUrl(nextPageUrl);
   }
+  function prevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  function goToPage(num) {
+    setCurrentPageUrl(`https://rickandmortyapi.com/api/character?page=${num}`);
+  }
+  if (loading) return "Loading...";
+
+  const charList = characters.map((char) => (
+    <Character
+      key={Math.floor(Math.random() * 10000)}
+      name={char.name}
+      img={char.image}
+    />
+  ));
+
   return (
-    <div className="App">
-      <ListaPostaci postaci={postaci} />
-    </div>
+    <Container>
+      <div className="char-cards">{charList}</div>
+      <Pagination
+        nextPage={nextPageUrl ? nextPage : null}
+        prevPage={prevPageUrl ? prevPage : null}
+        goToPage={goToPage}
+        pages={pages}
+      />
+    </Container>
   );
 }
 
---
+++ app/src/pages/components/Character.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Character = ({ name, img }) => {
+  return (
+    <div className="character-card">
+      <img src={img} alt="" />
+      <div>{name}</div>
+    </div>
+  );
+};
+export default Character;
