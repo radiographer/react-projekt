948f083febdabbe106fa53c0eb2953d1cbef6fc6
radiographer <lukas.w@gmx.com>
2021-08-24 13:41:07 +0200

    details

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,42 +1,7 @@
 import React from "react";
 
-import { Grid, TextField, Button } from "@material-ui/core";
-
-export default function App() {
-  return (
-    <Grid
-      container
-      justify="center"
-      alignItems="center"
-      direction="column"
-      style={{ minHeight: "100vh" }}
-      spacing={5}
-    >
-      <Grid item style={{ border: "0.2px solid gray" }}>
-        <Login />
-      </Grid>
-    </Grid>
-  );
+function Login() {
+  return <div></div>;
 }
 
-const Login = () => {
-  return (
-    <Grid container direction="column" alignItems="center" justify="center">
-      <TextField
-        variant="outlined"
-        label="Email"
-        fullWidth
-        style={{ marginBottom: "2em" }}
-      />
-      <TextField
-        variant="outlined"
-        label="Password"
-        fullWidth
-        style={{ marginBottom: "2em" }}
-      />
-      <Button size="large" variant="contained" color="primary">
-        LOGIN
-      </Button>
-    </Grid>
-  );
-};
+export default Login;
